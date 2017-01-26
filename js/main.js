80517d39104454259e46c8ca07ce34a8725c108a
Brandon Roth <brandon.roth892@gmail.com>
2017-01-26 12:11:03 -0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var bgColorArray = ["#16a085", "#27ae60", "#2c3e50", "#f39c12", "#e74c3c", "#9b59b6", "#FB6964", "#342224", "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+var jokeQuote = '';
+
+function callQuote() {
+  
+  $.getJSON("http://api.icndb.com/jokes/random", function(json) {
+    jokeQuote = (json.value.joke);
+  
+    $(".chucktainer").animate({
+      opacity: 0
+      }, 500,
+    function() {
+      $(".chucktainer").animate({
+        opacity: 1
+        }, 500);
+      
+    $("#quote").html(JSON.stringify(jokeQuote));
+     });
+    
+//    var selectBg = Math.floor(Math.random() * bgColorArray.length);
+//    $("html body").animate({
+//        backgroundColor: bgColorArray[selectBg],
+//        color: bgColorArray[selectBg]
+//      }, 1000);
+//    $(".button").animate({
+//        backgroundColor: bgColorArray[selectBg]
+//      }, 1000);
+  });
+}   
+
+$(document).ready(function() {
+  callQuote();
+  $("#newchuck").on("click", callQuote); 
+  $('#tweet').on('click', function() {
+    window.open('https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent(jokeQuote));
+  });
+});
